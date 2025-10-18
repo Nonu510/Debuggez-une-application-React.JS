046eaaa6b497ca5f2391a1f7d7650e85c502a2c8
Nolwenn Dubourg <nold14@hotamil.fr>
2025-10-18 10:18:45 -0300

    ajout de la modale au clic de l'événement du footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import Logo from "../../components/Logo";
 import Icon from "../../components/Icon";
 import Form from "../../containers/Form";
 import Modal from "../../containers/Modal";
+import ModalEvent from "../../containers/ModalEvent";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
@@ -116,15 +117,20 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-              {last && (
-        <EventCard
-          imageSrc={last.cover}
-          title={last.title}
-          date={new Date(last.date)}
-          small
-          label="boom"
-        />
-      )}
+        {last && (
+          <Modal Content={<ModalEvent event={last} />}>
+            {({ setIsOpened }) => (
+              <EventCard
+                onClick={() => setIsOpened(true)}
+                imageSrc={last.cover}
+                title={last.title}
+                date={new Date(last.date)}
+                small
+                label={last.type}
+              />
+            )}
+          </Modal>
+        )}
 
       </div>
       <div className="col contact">
